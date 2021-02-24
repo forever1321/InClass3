d9123a71a606c88288a18f6424e8735e514c38c6
forever1321 <70979696+forever1321@users.noreply.github.com>
2021-02-23 21:47:10 -0500

    Create Hello.java

---
+++ src/main/java/servlet/Hello.java
@@ -0,0 +1,28 @@
+// Simple example servlet from slides
+import javax.servlet.*; // servlet library
+import javax.servlet.http.*; // servlet library
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+
+// The @WebServletannotation is used to declare a servlet
+@WebServlet(name = "FirstServlet", urlPatterns = {"/sbhushaHello"})
+public class Hello extends HttpServlet // Inheriting from HttpServlet makes this a servlet
+{
+public void doGet (HttpServletRequest request, HttpServletResponse response)
+                   throws ServletException, IOException
+{
+   response.setContentType("text/html"); // Tells the web container what we're sending back
+   PrintWriter out = response.getWriter(); // Make it appear as if we're "writing" to the browser window
+
+   out.println("<html>");
+   out.println("<head>");
+   out.println("<title>Servlet example</title>");
+   out.println("</head>");
+   out.println("<body>");
+   out.println("<p>My first servlet.</p>");
+   out.println("<p>Hello Moto. I am Shashwat. Hello Moto Hello Moto.</p>");
+   out.println("</body>");
+   out.println("</html>");
+   out.close();
+}  // end doGet()
+}  // end Hello
